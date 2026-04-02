01699266380cb048ccd8da3448499829537928c9
Mena <dayjacs03@gmail.com>
2026-04-02 18:04:16 -0500

    setting up bare min routeS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from ".index";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={routes}></RouterProvider>
+  </React.StrictMode>,
+);
