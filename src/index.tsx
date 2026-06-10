1b742c4452dc4b378ea1873bdb023f03ba75ec84
Mena <dayjacs03@gmail.com>
2026-06-10 17:56:57 -0500

    adding global reset w cssbaseline

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
 import { Container, Box, Grid, Typography, CssBaseline } from "@mui/material";
 import Img from "./imgs/one.jpg";
 import Fakeheadshot from "./imgs/two.jpg";
-import "./global.css";
 
 function App() {
   return (
     <>
       <CssBaseline />
       <Box sx={{ overflowX: "hidden" }}>
-      <Box sx={{ height: "300px", backgroundColor: "green" }}>
-        <img
-          style={{ height: "100%", width: "100%", objectFit: "cover" }}
-          src={Img}
-          alt="header image of green lilly pads"
-        />
-      </Box>
-
-      <Container>
-        <Box>
-          <img src={Fakeheadshot} alt="Mena Jackson picture" />
+        <Box sx={{ height: "300px", backgroundColor: "green" }}>
+          <img
+            style={{ height: "100%", width: "100%", objectFit: "cover" }}
+            src={Img}
+            alt="header image of green lilly pads"
+          />
         </Box>
-      </Container>
-    </Box>
+
+        <Container>
+          <Box>
+            <img src={Fakeheadshot} alt="Mena Jackson picture" />
+          </Box>
+        </Container>
+      </Box>
+    </>
   );
 }
 
