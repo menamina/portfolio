4f67730b817b6b23c03089ac0806357356e0ad63
Mena <dayjacs03@gmail.com>
2026-06-10 18:49:55 -0500

    adding global reset w cssbaseline

--- src/components/portfolio.jsx
+++
@@ -1,3 +0,0 @@
-function Portfolio() {}
-
-export default Portfolio;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import { Container, Box, Grid, Typography, CssBaseline } from "@mui/material";
 import Img from "./imgs/one.jpg";
 import Fakeheadshot from "./imgs/two.jpg";
 
+const underlines = {
+  width: "300px",
+  borderBottom: "1px solid #d3d1cb",
+  padding: "8px",
+};
+
 function App() {
   return (
     <>
@@ -21,13 +27,44 @@ function App() {
           />
         </Box>
 
-        <Container sx={{ width: "100%", bgcolor: "yellow" }}>
+        <Container
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: "30px",
+            paddingBottom: "75px",
+          }}
+        >
           <Box sx={{ width: "inherit" }}>
             <img
               src={Fakeheadshot}
               alt="Mena Jackson picture"
-              style={{ height: "100%", width: "400px", objectFit: "cover" }}
+              style={{
+                height: "400px",
+                width: "400px",
+                objectFit: "cover",
+                borderRadius: "100%",
+              }}
             />
+          </Box>
+          <Typography>
+            <Typography variant="h3">Mena Jackson. </Typography>
+            <Typography variant="h6">
+              <br></br> Full stack web developer bringing visions to life
+              <br></br> one line at a time.
+            </Typography>
+          </Typography>
+          <Box sx={{ display: "flex", gap: "40px" }}>
+            <Box>
+              <Typography variant="h5">Overview</Typography>
+              <Box sx={underlines}></Box>
+            </Box>
+            <Box>
+              <Typography variant="h5">Portfolio</Typography>
+              <Box sx={underlines}></Box>
+            </Box>
           </Box>
         </Container>
       </Box>
