487b89b7d1df2960cc79c5c33e961e99fb68cdc5
Mena <dayjacs03@gmail.com>
2026-06-10 17:56:23 -0500

    header work

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
-import { Container, Box, Grid } from "@mui/material";
+import { Container, Box, Grid, Typography, CssBaseline } from "@mui/material";
 import Img from "./imgs/one.jpg";
+import Fakeheadshot from "./imgs/two.jpg";
 import "./global.css";
 
 function App() {
   return (
-    <Box sx={{ overflowX: "hidden" }}>
-      <Box sx={{ height: "100px" }}>
-        <img style={{ height: "100%", object-fit: cover }} src={Img} alt="header image of green lilly pads" />
+    <>
+      <CssBaseline />
+      <Box sx={{ overflowX: "hidden" }}>
+      <Box sx={{ height: "300px", backgroundColor: "green" }}>
+        <img
+          style={{ height: "100%", width: "100%", objectFit: "cover" }}
+          src={Img}
+          alt="header image of green lilly pads"
+        />
       </Box>
+
+      <Container>
+        <Box>
+          <img src={Fakeheadshot} alt="Mena Jackson picture" />
+        </Box>
+      </Container>
     </Box>
   );
 }
