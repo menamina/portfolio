f6abfc84294f8f42114424591ff151978900a605
Mena <dayjacs03@gmail.com>
2026-06-10 17:05:00 -0500

    portfolio

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,14 @@
 import { Link, Outlet } from "react-router-dom";
+import { Container } from "@mui/material";
 
 function App() {
   return (
-    <div className="mainDIV">
-      <div className="nav">
-        <div>Mena.</div>
-        <div>
-          <Link to="/portfolio">PORTFOLIO</Link>
-          <Link to="/about">ABOUT</Link>
-          <Link to="/contact">CONTACT</Link>
-        </div>
-      </div>
-      <div className="rendering-routes">
-        <Outlet></Outlet>
-      </div>
-    </div>
+    <Container>
+      <box>
+        <img src={} alt="header image of"/>
+      </box>
+      <Grid container></Grid>
+    </Container>
   );
 }
 
