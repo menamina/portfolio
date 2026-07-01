df9d2b1cffa7808e10d3358e6b91837381746ad5
Mena <dayjacs03@gmail.com>
2026-07-01 14:40:12 -0500

    img carousel

--- src/components/imgCarousel.tsx
+++ src/components/imgCarousel.tsx
@@ -9,3 +9,17 @@ import Wrongpass from "../appIMGS/wrongpass.png";
 import Edit from "../appIMGS/edit.png";
 import Comment from "../appIMGS/comment.png";
 import Inven from "../appIMGS/inven.png";
+
+function ImgCarousel({ imgs }: { imgs: string[] }) {
+  return (
+    <Carousel swipeable={true}>
+      {imgs.map((img, index) => (
+        <div key={index}>
+          <img src={img} />
+        </div>
+      ))}
+    </Carousel>
+  );
+}
+
+export default ImgCarousel;
