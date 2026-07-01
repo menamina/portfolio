3c059a7218d582a368541620e44814760968f94f
Mena <dayjacs03@gmail.com>
2026-07-01 14:34:23 -0500

    img carousel

---
+++ src/components/imgCarousel.tsx
@@ -0,0 +1,11 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import Feed from "../appIMGS/feed.png";
+import Profile from "../appIMGS/profile.png";
+import Settings from "../appIMGS/set.png";
+import Pass from "../appIMGS/pass.png";
+import Wrongpass from "../appIMGS/wrongpass.png";
+import Edit from "../appIMGS/edit.png";
+import Comment from "../appIMGS/comment.png";
+import Inven from "../appIMGS/inven.png";
