59c12254438ddb98557a0ef28ddab684c0fd05f5
Mena <dayjacs03@gmail.com>
2026-09-03 07:03:54 -0500

    updating portfolio

--- src/components/projComp.tsx
+++ src/components/projComp.tsx
@@ -1,3 +1,30 @@
-function Project({}) {}
+import { Typography } from "@mui/material";
+
+function Project({ title, description1, description2 = null, tech, pics }) {
+  return (
+    <Box>
+      <Typography>{title}</Typography>
+      <Typography>{description1}</Typography>
+      {description2 && <Typography>{description2}</Typography>}
+      <Typography>Build:</Typography>
+        <Box
+        component="ul"
+        sx={{
+          "& li::marker": {
+            content: '"☆ "',
+          },
+        }}
+      >
+        
+      </Box>
+      {tech.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+
+      <ImgCarousel imgs={pics} />
+    </Box>
+    </Box>
+  );
+}
 
 export default Project;
